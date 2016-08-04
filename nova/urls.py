1c9a3a36e5f2b404ce3903eddc9ada87896fd5d8
Jongwon(Jerry) Han <addnull@gmail.com>
2016-08-05 05:56:24 +0900

    add an endpoint '/echo/'.

--- nova/urls.py
+++ nova/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import api_echo.views
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+api_echo.views.add_urlpatterns(urlpatterns)
